1ef496cde68527eae10d38f0660e48db635f2af9
BuildTools <unconfigured@null.spigotmc.org>
2020-11-18 22:49:51 -0600

    Testing adc reading function

--- Soil_Calibration.py
+++ Soil_Calibration.py
@@ -29,11 +29,17 @@ if (os.path.isfile(PATH) == False): #check if file already exists
     with open('Pinout.ini', 'w') as configfile: #open pinout.ini as file object
         Config.write(configfile) #save ini file
 
-Config.read(PATH) #begin reading the config file
-ADC_PIN = int(Config.get('Pin_Values', 'ADC_PIN')) #set ADC_PIN to value read in config file
-ADC_GAIN = int(Config.get('Pin_Values', 'ADC_GAIN')) #set ADC_GAIN to value read in config file
-adc = Adafruit_ADS1x15.ADS1115() #store ADC class to variable
-
+##Create a function for reading the ADC
+def adc_read(retry=1):
+    Config.read(PATH) #begin reading the config file
+    ADC_PIN = int(Config.get('Pin_Values', 'ADC_PIN')) #set ADC_PIN to value read in config file
+    ADC_GAIN = int(Config.get('Pin_Values', 'ADC_GAIN')) #set ADC_GAIN to value read in config file
+    adc = Adafruit_ADS1x15.ADS1115() #store ADC class to variable
+    readings = []
+    for i in range(1, retry):
+        readings.append(adc.read_adc(ADC_PIN, gain=ADC_GAIN))
+        print(readings[i])
+    return mean(readings)
 
 print("Now beginning soil sensor calibration...\n")
 
