211f2916a4e54334955d3e69fd0ec07c2971c1a1
Cadell Van Den Heuvel <cadellvandenheuvel@gmail.com>
2020-11-19 18:01:49 -0600

    Create helpers.py
    adding file for small helper functions

---
+++ helpers.py
@@ -0,0 +1,4 @@
+import Adafruit_ADS1x15 #soil moisture sensor
+import os #tools for working with the CLI
+from configparser import ConfigParser #ini file manipulation
+from statistics import mean #math
