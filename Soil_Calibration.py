b33297ecca7f598b461e1d268d742f6e6c37aad1
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 22:19:02 -0600

    updated soil calibration and helpers function

--- Soil_Calibration.py
+++ Soil_Calibration.py
@@ -3,36 +3,13 @@ import csv #file output
 import os #tools for working with the CLI
 import numpy #math
 from configparser import ConfigParser #ini file manipulation
-from helpers.py import adc_read #import adc read function
-
-#Get current directory for log files and for pin file
-PROJECT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-path = "%s/Pinout.ini" % PROJECT_DIRECTORY
-
-Config = ConfigParser()
-if (os.path.isfile(path) == False): #check if file already exists
-    Pinout = open(path, "w+") #create file if none exists
-    Pinout.close()
-    Config.add_section('Pin_Values')
-    Config.add_section('Address_Values')
-    Config.add_section('Calibration_Constants')
-    Config.set('Pin_Values', 'FAN_ONE', '13') #set value of FAN_ONE in ini file
-    Config.set('Pin_Values', 'FAN_TWO', '12') #set value of FAN_TWO in ini file
-    Config.set('Pin_Values', 'ADC_PIN', '1') #set value of ADC_PIN in ini file
-    Config.set('Pin_Values', 'ADC_GAIN', '1') #set value of ADC_GAIN in ini file
-    Config.set('Pin_Values', 'PUMP', '17') #set value of PUMP in ini file
-    Config.set('Pin_Values', 'LIGHT', '27') #set value of LIGHT in ini file
-    Config.set('Pin_Values', 'FLOAT', '4') #set value of FLOAT in ini file
-    Config.set('Pin_Values', 'TEMP', '23') #set value of TEMP in ini file
-    Config.set('Pin_Values', 'BUTTON_ONE', '6') #set value of BUTTON_ONE in ini file
-    Config.set('Pin_Values', 'BUTTON_TWO', '16') #set value of BUTTON_TWO in ini file
-    Config.set('Pin_Values', 'BUTTON_THREE', '26') #set value of BUTTON_THREE in ini file
-    Config.set('Address_Values', 'OLED', '0x3c') #set value of OLED in ini file
-    with open('Pinout.ini', 'w') as configfile: #open pinout.ini as file object
-        Config.write(configfile) #save ini file
+from helpers import adc_read #import adc read function
+from helpers import pinout_init #import pinout initialization
 
 print("Now beginning soil sensor calibration...\n")
+pinout_init()
 
+PROJECT_DIRECTORY = os.path.dirname(os.path.realpath(__file__)) #Get current directory for log files and for pin file
 path = "%s/Calibration.csv" % PROJECT_DIRECTORY
 if(os.path.isfile(path) == False):
     Calibration = open(path, "w+") #create file if none exists
