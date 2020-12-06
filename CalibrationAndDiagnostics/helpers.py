aef5262e91d45d433124d41d74369a33c37472f1
CadellVDH <cadellvandenheuvel@gmail.com>
2020-12-06 17:44:20 -0600

    dbg

--- CalibrationAndDiagnostics/helpers.py
+++ CalibrationAndDiagnostics/helpers.py
@@ -18,42 +18,41 @@ class pinout():
     ##Create an initialization function for creating a default pinout file
     def __init__(self):
         if (os.path.isfile(pinout.PATH) == False): #check if file already exists
-            Pinout = open(pinout.PATH, "w+") #create file if none exists
-            Pinout.close()
-            Config = ConfigParser()
-            Config.add_section('Pin_Values')
-            Config.add_section('Address_Values')
-            Config.add_section('Calibration_Constants')
-            Config.set('Pin_Values', 'FAN_ONE', '13') #set value of FAN_ONE in ini file
-            Config.set('Pin_Values', 'FAN_TWO', '12') #set value of FAN_TWO in ini file
-            Config.set('Pin_Values', 'ADC_PIN', '1') #set value of ADC_PIN in ini file
-            Config.set('Pin_Values', 'ADC_GAIN', '1') #set value of ADC_GAIN in ini file
-            Config.set('Pin_Values', 'PUMP', '17') #set value of PUMP in ini file
-            Config.set('Pin_Values', 'LIGHT', '27') #set value of LIGHT in ini file
-            Config.set('Pin_Values', 'FLOAT', '4') #set value of FLOAT in ini file
-            Config.set('Pin_Values', 'TEMP', '23') #set value of TEMP in ini file
-            Config.set('Pin_Values', 'BUTTON_ONE', '6') #set value of BUTTON_ONE in ini file
-            Config.set('Pin_Values', 'BUTTON_TWO', '16') #set value of BUTTON_TWO in ini file
-            Config.set('Pin_Values', 'BUTTON_THREE', '26') #set value of BUTTON_THREE in ini file
-            Config.set('Address_Values', 'OLED', '0x3c') #set value of OLED in ini file
+            self.Pinout = open(pinout.PATH, "w+") #create file if none exists
+            self.Pinout.close()
+            self.Config = ConfigParser()
+            self.Config.add_section('Pin_Values')
+            self.Config.add_section('Address_Values')
+            self.Config.add_section('Calibration_Constants')
+            self.Config.set('Pin_Values', 'FAN_ONE', '13') #set value of FAN_ONE in ini file
+            self.Config.set('Pin_Values', 'FAN_TWO', '12') #set value of FAN_TWO in ini file
+            self.Config.set('Pin_Values', 'ADC_PIN', '1') #set value of ADC_PIN in ini file
+            self.Config.set('Pin_Values', 'ADC_GAIN', '1') #set value of ADC_GAIN in ini file
+            self.Config.set('Pin_Values', 'PUMP', '17') #set value of PUMP in ini file
+            self.Config.set('Pin_Values', 'LIGHT', '27') #set value of LIGHT in ini file
+            self.Config.set('Pin_Values', 'FLOAT', '4') #set value of FLOAT in ini file
+            self.Config.set('Pin_Values', 'TEMP', '23') #set value of TEMP in ini file
+            self.Config.set('Pin_Values', 'BUTTON_ONE', '6') #set value of BUTTON_ONE in ini file
+            self.Config.set('Pin_Values', 'BUTTON_TWO', '16') #set value of BUTTON_TWO in ini file
+            self.Config.set('Pin_Values', 'BUTTON_THREE', '26') #set value of BUTTON_THREE in ini file
+            self.Config.set('Address_Values', 'OLED', '0x3c') #set value of OLED in ini file
             with open('Pinout.ini', 'w') as configfile: #open pinout.ini as file object
-                Config.write(configfile) #save ini file
+                self.Config.write(configfile) #save ini file
 
     #Create a function for getting pins from pinout.ini file
     def getPin(self, pin):
-        Config = ConfigParser()
-        Config.read(pinout.PATH)
+        self.Config.read(pinout.PATH)
         try:
-            return int(Config.get('Pin_Values', pin)) #return pin based on pinout.ini file
+            return int(self.Config.get('Pin_Values', pin)) #return pin based on pinout.ini file
         except:
             return None
 
     #Create a function for getting address values from pinout.ini file
     def getAddr(self, device):
-        Config = ConfigParser()
-        Config.read(pinout.PATH)
+        self.Config = ConfigParser()
+        self.Config.read(pinout.PATH)
         try:
-            return int(Config.get('Address_Values', device), 0) #return base 0 address value
+            return int(self.Config.get('Address_Values', device), 0) #return base 0 address value
         except:
             return None
 
