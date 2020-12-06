0817824316f0c3245429d04b8e922e737a06f6dd
CadellVDH <cadellvandenheuvel@gmail.com>
2020-12-05 23:37:18 -0600

    Fake Message

--- Main.py
+++ Main.py
@@ -14,4 +14,4 @@ from helpers import * #import helper functions and classes
 pins = pinout() #initialize pinout
 oled = oled_utility(128, 32, pins.getAddr('OLED')) #initialize OLED display
 
-oled.write_center("Message", 10,title="Title")
+oled.write_center("70%", 8, title="Humidity")
