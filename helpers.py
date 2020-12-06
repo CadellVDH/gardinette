5c79b2db3f656dc8e1fc366977b22ceae2925f84
CadellVDH <cadellvandenheuvel@gmail.com>
2020-12-05 23:36:19 -0600

    dbg

--- helpers.py
+++ helpers.py
@@ -98,9 +98,9 @@ class oled_utility():
 
         self.font = ImageFont.truetype("%s/Hack-Regular.ttf" % self.PROJECT_DIRECTORY, 7) #get text font
 
-        (self.font_width, self.font_height) = self.font.getsize(message) #get font width and height
+        (self.font_width, self.font_height) = self.font.getsize(title) #get font width and height
         self.x_pos = self.width // 2 - self.font_width // 2 #move text to center
-        self.draw.text((self.x_pos, 0), message, font=self.font, fill=255) #draw message at position
+        self.draw.text((self.x_pos, 0), title, font=self.font, fill=255) #draw message at position
 
         self.font = ImageFont.truetype("%s/Hack-Regular.ttf" % self.PROJECT_DIRECTORY, font_size) #set to regular font size
 
