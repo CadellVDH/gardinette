60abf4390367a85d8a05217537ae12a91bc77172
CadellVDH <cadellvandenheuvel@gmail.com>
2021-04-05 00:36:57 -0500

    dbg

--- Main.py
+++ Main.py
@@ -61,7 +61,7 @@ dataGlanceThread.start() #start data quick display
 # fanControlThread = fanControl(FAN_ONE, FAN_TWO) #intialize fanControl object
 # fanControlThread.start() #start fanControl thread
 
-actuatorControlThread = actuatorControl()
+actuatorControlThread = actuatorControl(PUMP, LIGHT, FAN_ONE, FAN_TWO)
 actuatorControlThread.start()
 
 targetAdjustThread = targetAdjust() #initialize target adjustment thread
